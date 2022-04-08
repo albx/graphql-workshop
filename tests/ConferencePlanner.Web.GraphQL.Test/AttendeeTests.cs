c0a6bf0f33edd8b7c6764282ddda4874d2ab598a
Alberto Mori <a.mori@abletech.it>
2022-04-08 17:04:11 +0200

    Part 8 - adding tests

---
+++ tests/ConferencePlanner.Web.GraphQL.Test/AttendeeTests.cs
@@ -0,0 +1,80 @@
+﻿using ConferencePlanner.Data.Persistence;
+using ConferencePlanner.Web.GraphQL.Attendees;
+using ConferencePlanner.Web.GraphQL.Types;
+using HotChocolate;
+using HotChocolate.Execution;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Snapshooter.Xunit;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GraphQL.Tests
+{
+    public class AttendeeTests
+    {
+        [Fact]
+        public async Task Attendee_Schema_Changed()
+        {
+            // arrange
+            // act
+            ISchema schema = await new ServiceCollection()
+                .AddPooledDbContextFactory<ConferencePlannerDbContext>(
+                    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+                .AddGraphQL()
+                .AddQueryType(d => d.Name("Query"))
+                    .AddTypeExtension<AttendeeQueries>()
+                .AddMutationType(d => d.Name("Mutation"))
+                    .AddTypeExtension<AttendeeMutations>()
+                .AddType<AttendeeType>()
+                .AddType<SessionType>()
+                .AddType<SpeakerType>()
+                .AddType<TrackType>()
+                .AddGlobalObjectIdentification()
+                .BuildSchemaAsync();
+
+            // assert
+            schema.Print().MatchSnapshot();
+        }
+
+        [Fact]
+        public async Task RegisterAttendee()
+        {
+            // arrange
+            IRequestExecutor executor = await new ServiceCollection()
+                .AddPooledDbContextFactory<ConferencePlannerDbContext>(
+                    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+                .AddGraphQL()
+                .AddQueryType(d => d.Name("Query"))
+                    .AddTypeExtension<AttendeeQueries>()
+                .AddMutationType(d => d.Name("Mutation"))
+                    .AddTypeExtension<AttendeeMutations>()
+                .AddType<AttendeeType>()
+                .AddType<SessionType>()
+                .AddType<SpeakerType>()
+                .AddType<TrackType>()
+                .AddGlobalObjectIdentification()
+                .BuildRequestExecutorAsync();
+
+            // act
+            IExecutionResult result = await executor.ExecuteAsync(@"
+        mutation RegisterAttendee {
+            registerAttendee(
+                input: {
+                    emailAddress: ""michael@chillicream.com""
+                        firstName: ""michael""
+                        lastName: ""staib""
+                        userName: ""michael3""
+                    })
+            {
+                attendee {
+                    id
+                }
+            }
+        }");
+
+            // assert
+            result.ToJson().MatchSnapshot();
+        }
+    }
+}
